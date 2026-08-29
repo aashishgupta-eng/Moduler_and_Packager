3bd1c23f5b6fc428dcfd676848a1fb662d968cab
Aashish Gupta <aashishgupta335@gmail.com>
2026-08-29 10:49:57 +0530

    commit
    Explain the purpose of the mytools package and its structure.

--- mytools/__init__.py
+++ mytools/__init__.py
@@ -1,1 +1,7 @@
+'''
+This file makes the mytools folder a Python package.
 
+A package is a folder that contains Python modules.
+The __init__.py file tells Python that this folder
+can be used as a package.
+'''
